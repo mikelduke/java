d4539a507f0c402d226869f6a12a2a6e23b51cae
Mikel Duke <mikelduke@mdp3.net>
2014-07-18 22:10:37 -0500

    Add Midi out test and forwarding
    Added Midi Out test function
    Added forwarding for all inputs to all outputs

--- MIDITest/src/net/mdp3/java/MIDITest/MIDITest.java
+++ MIDITest/src/net/mdp3/java/MIDITest/MIDITest.java
@@ -8,10 +8,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Transmitter;
 
 /**
@@ -21,9 +23,11 @@ import javax.sound.midi.Transmitter;
 public class MIDITest {
 	
 	private BufferedReader br;
-	private LinkedList<Receiver> receivers = new LinkedList<Receiver>();
+	//private LinkedList<Receiver> receivers = new LinkedList<Receiver>();
+	MIDITest_Receiver mtr = new MIDITest_Receiver("MultiIn");
 	private LinkedList<MidiDevice> openDevices = new LinkedList<MidiDevice>();
 	private LinkedList<Transmitter> transmitters = new LinkedList<Transmitter>();
+	private LinkedList<Receiver> midiOutDevices = new LinkedList<Receiver>();
 
 	/**
 	 * @param args
@@ -41,6 +45,13 @@ public class MIDITest {
 			System.out.print("MIDITest: ");
 			try {
 				inStr = br.readLine();
+				
+				if (inStr.toLowerCase().indexOf("test") > -1 && !midiOutDevices.isEmpty()) {
+					sendTestMsg();
+				}
+				else if (inStr.toLowerCase().indexOf("forward") > -1 && !midiOutDevices.isEmpty()) {
+					forwardAllMidi();
+				}
 			} catch (IOException e) {
 				System.out.println("Error reading input! " + e);
 				e.printStackTrace();
@@ -68,12 +79,23 @@ public class MIDITest {
 						device.open();
 						openDevices.addLast(device);
 						
-						
 						Transmitter t = device.getTransmitter();
 						transmitters.addLast(t);
 						
-						receivers.add(new MIDITest_Receiver(infos[i].getName()));
-						t.setReceiver(receivers.getLast());
+						//receivers.add(new MIDITest_Receiver(infos[i].getName()));
+						//t.setReceiver(receivers.getLast());
+						t.setReceiver(mtr);
+					}
+				}
+				else if (device.getClass().getName().equalsIgnoreCase("com.sun.media.sound.MidiOutDevice")
+						/* && infos[i].getName().toLowerCase().indexOf("usb") > -1*/) {
+					System.out.println("USB MIDI Out Found!");
+					if (!device.isOpen() && device.getMaxReceivers() != 0) {
+						device.open();
+						openDevices.addLast(device);
+						
+						Receiver r = device.getReceiver();
+						midiOutDevices.add(r);
 					}
 				}
 		    	device = null;
@@ -84,11 +106,39 @@ public class MIDITest {
 		}
 	}
 	
+	private void sendTestMsg() {
+		Receiver r = null;
+		ShortMessage message = new ShortMessage();
+		long timeStamp = -1;
+		
+		try {
+			message.setMessage(ShortMessage.NOTE_ON, 2, 60, 127);
+		} catch (InvalidMidiDataException e) {
+			System.out.println("Error creating midi message: " + e);
+			e.printStackTrace();
+		}
+		
+		System.out.println("Sending message: " + message.getChannel() + " " + 
+				message.getCommand() + " " + message.getData1() + " " + message.getData2());
+		for (int i = 0; i < midiOutDevices.size(); i++) {
+			r = midiOutDevices.get(i);
+			
+			r.send(message, timeStamp);
+		}
+	}
+	
+	private void forwardAllMidi() {
+		for (int i = 0; i < midiOutDevices.size(); i++)
+			mtr.addForwarder(midiOutDevices.get(i));
+	}
+	
 	public void closeAll() {
-		for (int i = 0; i < receivers.size(); i++)
-			receivers.get(i).close();
+		//for (int i = 0; i < receivers.size(); i++)
+			//receivers.get(i).close();
 		for (int i = 0; i < transmitters.size(); i++)
 			transmitters.get(i).close();
+		for (int i = 0; i < midiOutDevices.size(); i++)
+			midiOutDevices.get(i).close();
 		for (int i = 0; i < openDevices.size(); i++)
 			openDevices.get(i).close();
 	}
--- MIDITest/src/net/mdp3/java/MIDITest/MIDITest_Receiver.java
+++ MIDITest/src/net/mdp3/java/MIDITest/MIDITest_Receiver.java
@@ -3,8 +3,11 @@
  */
 package net.mdp3.java.MIDITest;
 
+import java.util.LinkedList;
+
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
 
 /**
  * @author Mikel
@@ -12,6 +15,8 @@ import javax.sound.midi.Receiver;
  */
 public class MIDITest_Receiver implements Receiver {
 	private String recName = "";
+	
+	private LinkedList<Receiver> receivers = new LinkedList<Receiver>();
 	
 	public MIDITest_Receiver(String name) {
 		this.recName = name;
@@ -25,7 +30,15 @@ public class MIDITest_Receiver implements Receiver {
 
 	@Override
 	public void send(MidiMessage arg0, long arg1) {
-		// TODO Auto-generated method stub
-		System.out.println(this.recName + " MIDIMessage: " + arg0 + " Time: " + arg1);
+		System.out.println(this.recName + " MIDIMessage: " + arg0.getMessage());
+		
+		if (!receivers.isEmpty()) {
+			for (int i = 0; i < receivers.size(); i++)
+				receivers.get(i).send(arg0, arg1);
+		}
+	}
+	
+	public void addForwarder(Receiver r) {
+		receivers.addLast(r);
 	}
 }
